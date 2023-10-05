1663ab6fefe8e52d07bd8fb277f0820f2e624cdc
brocahontaz <johan@rooter.se>
2023-10-05 18:11:50 +0200

    lots more initttt

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import * as core from "@actions/core"
+
+export async function run(): Promise<void> {
+  try {
+    const paths: string = core.getInput("paths")
+
+    const pathsList: string[] = paths.split(",")
+
+    console.log(pathsList)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
