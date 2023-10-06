f5fb23c501371aae42d395c7d01ecc5d0f7567fe
Johan Fjällbark <johan.fjallbark@gmail.com>
2023-10-06 09:54:11 +0200

    feat: dependabot file (#3)
    ### **Why** is the change needed?
    
    <!-- Add your reasons here -->
    Create the actual functionality of the action.
    
    ### **How** was the need addressed?
    
    <!-- Add the main changes made in the PR here -->
    - Read dependabot file.
    - Additional inputs for e.g. schedule, registries.
    - Generate configs.
    - Write configs to dependabot file.
    
    ### Concerns / side-effects of your changes
    
    <!-- Things like performance implications, user impact, tech debt etc.
    -->
    
    ### Checklist
    
    - [x] Addressed concerns / side-effects of your changes
    - [x] These changes can be promoted to production immediately
    - [x] I have tested the changes
    
    ### How to test this change
    
    <!-- Describe the steps for reviewers to test this change on their side
    -->

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,105 @@
 import * as core from "@actions/core"
+import fs from "fs/promises"
+import { glob } from "glob"
+import YAML from "yaml"
+import path from "path"
+
+const DEPENDABOT_FILE = "./.github/dependabot.yml"
+
+const getDependabotFile = async () => {
+  let file
+
+  try {
+    file = await fs.readFile(DEPENDABOT_FILE)
+
+    return file
+  } catch (error) {
+    file = `
+  version: 2
+  updates: []
+  registries: {}`
+
+    return file
+  }
+}
+
+const extractPaths = async (list: string[]) => {
+  const files = await glob(list)
+  const paths = [...new Set(files.map((file) => path.dirname(file)))]
+
+  return paths
+}
+
+const getPaths = async (type: string) => {
+  const input: string = core.getInput(type)
+  const inputList: string[] = input.split(",")
+  const paths = await extractPaths(inputList)
+
+  return paths
+}
+
+const buildConfigs = async (
+  paths: string[],
+  ecosystem: string,
+  registries: string,
+  schedule: string,
+) => {
+  const configs = paths.map((path) => ({
+    "package-ecosystem": `${ecosystem}`,
+    directory: `${path}/`,
+    ...(registries != "" && { registries }),
+    schedule: { interval: `${schedule}` },
+  }))
+
+  configs.sort()
+
+  return configs
+}
 
 export async function run(): Promise<void> {
   try {
-    const npmPaths: string = core.getInput("npm-paths")
-    const npmPathsList: string[] = npmPaths.split(",")
+    const dependabotFile = await getDependabotFile()
+    const currentDocument = YAML.parseDocument(dependabotFile.toString())
+    const state = currentDocument.toJS()
 
-    const actionPaths: string = core.getInput("action-paths")
-    const actionPathsList: string[] = actionPaths.split(",")
+    const npmPaths: string[] = await getPaths("npm-paths")
+    const actionPaths: string[] = await getPaths("action-paths")
+    const tfPaths: string[] = await getPaths("tf-paths")
 
-    const tfPaths: string = core.getInput("tf-paths")
-    const tfPathsList: string[] = tfPaths.split(",")
+    const registries = core.getInput("registries")
+    console.log("REGISTRIES", registries)
 
-    console.log(npmPathsList, actionPathsList, tfPathsList)
+    const npmConfigs = await buildConfigs(
+      npmPaths,
+      "npm",
+      "",
+      core.getInput("npm-schedule"),
+    )
+
+    const actionConfigs = await buildConfigs(
+      actionPaths,
+      "github-actions",
+      "",
+      core.getInput("action-schedule"),
+    )
+
+    const tfConfigs = await buildConfigs(
+      tfPaths,
+      "terraform",
+      core.getInput("tf-registries"),
+      core.getInput("tf-schedule"),
+    )
+
+    const allConfigs = [...npmConfigs, ...actionConfigs, ...tfConfigs]
+
+    state.updates = allConfigs
+
+    console.log("PRE", state.registries)
+    state.registries = registries
+    console.log("POST", state.registries)
+
+    const newDocument = new YAML.Document(state)
+    await fs.writeFile(DEPENDABOT_FILE, String(newDocument))
   } catch (error) {
     console.error(error)
   }
